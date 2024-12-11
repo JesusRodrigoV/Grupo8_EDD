c39768dbb228a3914b80d739cce9eea6f5aaa4b5
Manuel Franco Jiménez Mendoza <142540969+Atccn0206AI@users.noreply.github.com>
2024-12-10 23:01:46 -0400

    Update Producto.java

--- src/main/java/com/farmacia/model/Producto.java
+++ src/main/java/com/farmacia/model/Producto.java
@@ -1,32 +1,50 @@
 package com.farmacia.model;
 
-import java.util.Date;
+import java.util.Objects;
+import java.util.Random;
 
 public class Producto {
     private int id;
     private String nombre;
     private String descripcion;
     private double precio;
-    private String codigoBarras;
-    private Date fechaVencimiento;
-    private int stock;
-    private String numeroLote;
+    private int cantidadEnStock;
+    private String fechaDeVencimiento;
+    private int numeroDeLote;
 
-    public Producto(String nombre, String descripcion, double precio, String codigoBarras,
-                    Date fechaVencimiento, int stock, String numeroLote) {
-        this.id = generarId(codigoBarras);
+
+    public Producto() {
+        this.id = generarIdAleatorio();
+    }
+
+
+    public Producto(String nombre, String descripcion, double precio, int cantidadEnStock, String fechaDeVencimiento, int numeroDeLote) {
+        this.id = generarIdAleatorio();
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
-        this.codigoBarras = codigoBarras;
-        this.fechaVencimiento = fechaVencimiento;
-        this.stock = stock;
-        this.numeroLote = numeroLote;
+        this.cantidadEnStock = cantidadEnStock;
+        this.fechaDeVencimiento = fechaDeVencimiento;
+        this.numeroDeLote = numeroDeLote;
     }
 
-    private int generarId(String codigoBarras) {
-        return codigoBarras.hashCode();
+
+    public Producto(int id, String nombre, String descripcion, double precio, int cantidadEnStock, String fechaDeVencimiento, int numeroDeLote) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidadEnStock = cantidadEnStock;
+        this.fechaDeVencimiento = fechaDeVencimiento;
+        this.numeroDeLote = numeroDeLote;
     }
+
+  
+    private int generarIdAleatorio() {
+        Random random = new Random();
+        return random.nextInt(100000); 
+    }
+
 
     public int getId() {
         return id;
@@ -44,25 +62,64 @@ public class Producto {
         return precio;
     }
 
-    public String getCodigoBarras() {
-        return codigoBarras;
+    public int getCantidadEnStock() {
+        return cantidadEnStock;
     }
 
-    public Date getFechaVencimiento() {
-        return fechaVencimiento;
+    public String getFechaDeVencimiento() {
+        return fechaDeVencimiento;
     }
 
-    public int getStock() {
-        return stock;
+    public int getNumeroDeLote() {
+        return numeroDeLote;
     }
 
-    public String getNumeroLote() {
-        return numeroLote;
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setCantidadEnStock(int cantidadEnStock) {
+        this.cantidadEnStock = cantidadEnStock;
+    }
+
+    public void setFechaDeVencimiento(String fechaDeVencimiento) {
+        this.fechaDeVencimiento = fechaDeVencimiento;
+    }
+
+    public void setNumeroDeLote(int numeroDeLote) {
+        this.numeroDeLote = numeroDeLote;
     }
 
     @Override
     public String toString() {
         return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
-                + ", codigoBarras=" + codigoBarras + ", stock=" + stock + ", numeroLote=" + numeroLote + "]";
+                + ", cantidadEnStock=" + cantidadEnStock + ", fechaDeVencimiento=" + fechaDeVencimiento
+                + ", numeroDeLote=" + numeroDeLote + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return id == producto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
